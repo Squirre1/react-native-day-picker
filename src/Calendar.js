791285a5977fb4ddd9a62708d16a71c986bd86b3
Владислав Кузнецов <kak60g@gmail.com>
2016-11-22 10:47:27 +0300

    added holidays colors

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,276 +1,276 @@
 'use strict';
 
 import React, {
-	PropTypes
-}               from 'react';
+    PropTypes
+} from 'react';
 import {
-	ListView,
-	StyleSheet,
-}               from 'react-native';
+    ListView,
+    StyleSheet,
+} from 'react-native';
 
-import Month    from './Month';
+import Month from './Month';
 
+console.disableYellowBox = true;
 
 export default class Calendar extends React.Component {
-	static defaultProps = {
-		startDate: new Date(),
-		monthsCount: 24,
-		onSelectionChange: () => {
-		},
+    static defaultProps = {
+        startDate: new Date(),
+        monthsCount: 24,
+        onSelectionChange: () => {
+        },
 
-		monthsLocale: ['January', 'February', 'March', 'April', 'May', 'June',
-			'July', 'August', 'September', 'October', 'November', 'December'],
-		weekDaysLocale: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+        monthsLocale: ['January', 'February', 'March', 'April', 'May', 'June',
+            'July', 'August', 'September', 'October', 'November', 'December'],
+        weekDaysLocale: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
 
-		width: 280,
+        width: 280,
 
-		bodyBackColor: 'white',
-		bodyTextColor: 'black',
-		headerSepColor: 'grey',
+        bodyBackColor: 'white',
+        bodyTextColor: 'black',
+        headerSepColor: 'grey',
 
-		monthTextColor: 'black',
+        dayCommonBackColor: 'white',
+        dayCommonTextColor: 'black',
 
-		dayCommonBackColor: 'white',
-		dayCommonTextColor: 'black',
+        dayDisabledBackColor: 'white',
+        dayDisabledTextColor: 'grey',
 
-		dayDisabledBackColor: 'white',
-		dayDisabledTextColor: 'grey',
+        daySelectedBackColor: '#4169e1',
+        daySelectedTextColor: 'white',
 
-		daySelectedBackColor: '#4169e1',
-		daySelectedTextColor: 'white',
+        dayInRangeBackColor: '#87cefa',
+        dayInRangeTextColor: 'black',
 
-		dayInRangeBackColor: '#87cefa',
-		dayInRangeTextColor: 'black',
+        isFutureDate: false,
+        rangeSelect: true
+    };
 
-		isFutureDate: false,
-		rangeSelect: true
-	};
+    static propTypes = {
+        selectFrom: PropTypes.instanceOf(Date),
+        selectTo: PropTypes.instanceOf(Date),
 
-	static propTypes = {
-		selectFrom: PropTypes.instanceOf(Date),
-		selectTo: PropTypes.instanceOf(Date),
+        monthsCount: PropTypes.number,
+        startDate: PropTypes.instanceOf(Date),
 
-		monthsCount: PropTypes.number,
-		startDate: PropTypes.instanceOf(Date),
+        monthsLocale: PropTypes.arrayOf(PropTypes.string),
+        weekDaysLocale: PropTypes.arrayOf(PropTypes.string),
+        startFromMonday: PropTypes.bool,
 
-		monthsLocale: PropTypes.arrayOf(PropTypes.string),
-		weekDaysLocale: PropTypes.arrayOf(PropTypes.string),
-		startFromMonday: PropTypes.bool,
+        onSelectionChange: PropTypes.func,
 
-		onSelectionChange: PropTypes.func,
+        width: PropTypes.number,
 
-		width: PropTypes.number,
+        bodyBackColor: PropTypes.string,
+        bodyTextColor: PropTypes.string,
+        headerSepColor: PropTypes.string,
 
-		bodyBackColor: PropTypes.string,
-		bodyTextColor: PropTypes.string,
-		headerSepColor: PropTypes.string,
+        daysTextColor: PropTypes.string,
+        holidaysTextColor: PropTypes.string,
 
-		monthTextColor: PropTypes.string,
+        dayCommonBackColor: PropTypes.string,
+        dayCommonTextColor: PropTypes.string,
 
-		dayCommonBackColor: PropTypes.string,
-		dayCommonTextColor: PropTypes.string,
+        dayDisabledBackColor: PropTypes.string,
+        dayDisabledTextColor: PropTypes.string,
 
-		dayDisabledBackColor: PropTypes.string,
-		dayDisabledTextColor: PropTypes.string,
+        daySelectedBackColor: PropTypes.string,
+        daySelectedTextColor: PropTypes.string,
 
-		daySelectedBackColor: PropTypes.string,
-		daySelectedTextColor: PropTypes.string,
+        dayInRangeBackColor: PropTypes.string,
+        dayInRangeTextColor: PropTypes.string,
 
-		dayInRangeBackColor: PropTypes.string,
-		dayInRangeTextColor: PropTypes.string,
+        isFutureDate: PropTypes.bool,
+        rangeSelect: PropTypes.bool
+    };
 
-		isFutureDate: PropTypes.bool,
-		rangeSelect: PropTypes.bool
-	};
+    constructor(props) {
+        super(props);
 
-	constructor(props) {
-		super(props);
+        this.changeSelection = this.changeSelection.bind(this);
+        this.generateMonths = this.generateMonths.bind(this);
 
-		this.changeSelection = this.changeSelection.bind(this);
-		this.generateMonths = this.generateMonths.bind(this);
+        let {selectFrom, selectTo, monthsCount, startDate} = this.props;
 
-		let {selectFrom, selectTo, monthsCount, startDate} = this.props;
+        this.selectFrom = selectFrom;
+        this.selectTo = selectTo;
+        this.months = this.generateMonths(monthsCount, startDate);
 
-		this.selectFrom = selectFrom;
-		this.selectTo = selectTo;
-		this.months = this.generateMonths(monthsCount, startDate);
+        var dataSource = new ListView.DataSource({rowHasChanged: this.rowHasChanged});
 
-		var dataSource = new ListView.DataSource({rowHasChanged: this.rowHasChanged});
+        this.state = {
+            dataSource: dataSource.cloneWithRows(this.months)
+        }
+    }
 
-		this.state = {
-			dataSource: dataSource.cloneWithRows(this.months)
-		}
-	}
+    rowHasChanged(r1, r2) {
+        for (var i = 0; i < r1.length; i++) {
+            if (r1[i].status !== r2[i].status && !r1[i].disabled) {
+                return true;
+            }
+        }
+    }
 
-	rowHasChanged(r1, r2) {
-		for (var i = 0; i < r1.length; i++) {
-			if (r1[i].status !== r2[i].status && !r1[i].disabled) {
-				return true;
-			}
-		}
-	}
+    generateMonths(count, startDate) {
+        var months = [];
+        var dateUTC;
+        var monthIterator = startDate;
+        var {isFutureDate, startFromMonday} = this.props;
 
-	generateMonths(count, startDate) {
-		var months = [];
-		var dateUTC;
-		var monthIterator = startDate;
-		var {isFutureDate, startFromMonday} = this.props;
+        var startUTC = Date.UTC(startDate.getYear(), startDate.getMonth(), startDate.getDate());
 
-		var startUTC = Date.UTC(startDate.getYear(), startDate.getMonth(), startDate.getDate());
+        for (var i = 0; i < count; i++) {
+            var month = this.getDates(monthIterator, startFromMonday);
 
-		for (var i = 0; i < count; i++) {
-			var month = this.getDates(monthIterator, startFromMonday);
+            months.push(month.map((day) => {
+                dateUTC = Date.UTC(day.getYear(), day.getMonth(), day.getDate());
+                return {
+                    date: day,
+                    status: this.getStatus(day, this.selectFrom, this.selectTo),
+                    disabled: day.getMonth() !== monthIterator.getMonth()
+                    || ((isFutureDate) ? startUTC > dateUTC : startUTC < dateUTC)
+                }
+            }));
 
-			months.push(month.map((day) => {
-				dateUTC = Date.UTC(day.getYear(), day.getMonth(), day.getDate());
-				return {
-					date: day,
-					status: this.getStatus(day, this.selectFrom, this.selectTo),
-					disabled: day.getMonth() !== monthIterator.getMonth()
-					|| ((isFutureDate) ? startUTC > dateUTC : startUTC < dateUTC)
-				}
-			}));
+            if (isFutureDate) {
+                monthIterator.setMonth(monthIterator.getMonth());
+            } else {
+                monthIterator.setMonth(monthIterator.getMonth() - 1);
+            }
+        }
 
-			if (isFutureDate) {
-				monthIterator.setMonth(monthIterator.getMonth() + 1);
-			} else {
-				monthIterator.setMonth(monthIterator.getMonth() - 1);
-			}
-		}
+        return months;
+    }
 
-		return months;
-	}
+    getDates(month, startFromMonday) {
+        month = new Date(month);
+        month.setDate(1);
 
-	getDates(month, startFromMonday) {
-		month = new Date(month);
-		month.setDate(1);
+        var delta = month.getDay();
+        if (startFromMonday) {
+            delta--;
+            if (delta === -1) delta = 6;
+        }
 
-		var delta = month.getDay();
-		if (startFromMonday) {
-			delta--;
-			if (delta === -1) delta = 6;
-		}
+        var startDate = new Date(month);
+        startDate.setDate(startDate.getDate() - delta);
 
-		var startDate = new Date(month);
-		startDate.setDate(startDate.getDate() - delta);
+        month.setMonth(month.getMonth() + 1);
+        month.setDate(0);
 
-		month.setMonth(month.getMonth() + 1);
-		month.setDate(0);
+        delta = 6 - month.getDay();
+        if (startFromMonday) {
+            delta++;
+            if (delta === 7) delta = 0;
+        }
 
-		delta = 6 - month.getDay();
-		if (startFromMonday) {
-			delta++;
-			if (delta === 7) delta = 0;
-		}
+        var lastDate = new Date(month);
+        lastDate.setDate(lastDate.getDate() + delta);
 
-		var lastDate = new Date(month);
-		lastDate.setDate(lastDate.getDate() + delta);
+        var allDates = [];
+        while (startDate <= lastDate) {
+            allDates.push(new Date(startDate));
+            startDate.setDate(startDate.getDate() + 1);
+        }
+        return allDates;
+    }
 
-		var allDates = [];
-		while (startDate <= lastDate) {
-			allDates.push(new Date(startDate));
-			startDate.setDate(startDate.getDate() + 1);
-		}
-		return allDates;
-	}
+    changeSelection(value) {
+        var {selectFrom, selectTo, months} = this;
 
-	changeSelection(value) {
-		var {selectFrom, selectTo, months} = this;
+        if (!selectFrom) {
+            selectFrom = value;
+        } else if (!selectTo) {
+            if (value > selectFrom) {
+                selectTo = value;
+            } else {
+                selectFrom = value;
+            }
+        } else if (selectFrom && selectTo) {
+            selectFrom = value;
+            selectTo = null;
+        }
 
-		if (!selectFrom) {
-			selectFrom = value;
-		} else if (!selectTo) {
-			if (value > selectFrom) {
-				selectTo = value;
-			} else {
-				selectFrom = value;
-			}
-		} else if (selectFrom && selectTo) {
-			selectFrom = value;
-			selectTo = null;
-		}
+        months = months.map((month) => {
+            return month.map((day) => {
+                return {
+                    date: day.date,
+                    status: this.getStatus(day.date, selectFrom, selectTo),
+                    disabled: day.disabled
+                }
+            })
+        });
 
-		months = months.map((month) => {
-			return month.map((day) => {
-				return {
-					date: day.date,
-					status: this.getStatus(day.date, selectFrom, selectTo),
-					disabled: day.disabled
-				}
-			})
-		});
+        if (this.props.rangeSelect) {
+            this.selectFrom = selectFrom;
+            this.selectTo = selectTo;
+        } else {
+            this.selectFrom = this.selectTo = selectFrom;
+        }
 
-		if (this.props.rangeSelect) {
-			this.selectFrom = selectFrom;
-			this.selectTo = selectTo;
-		} else {
-			this.selectFrom = this.selectTo = selectFrom;
-		}
+        this.months = months;
 
-		this.months = months;
+        this.props.onSelectionChange(value, this.prevValue);
+        this.prevValue = value;
 
-		this.props.onSelectionChange(value, this.prevValue);
-		this.prevValue = value;
+        this.setState({
+            dataSource: this.state.dataSource.cloneWithRows(months)
+        })
+    }
 
-		this.setState({
-			dataSource: this.state.dataSource.cloneWithRows(months)
-		});
-	}
+    getStatus(date, selectFrom, selectTo) {
+        if (selectFrom) {
+            if (selectFrom.toDateString() === date.toDateString()) {
+                return 'selected';
+            }
+        }
+        if (selectTo) {
+            if (selectTo.toDateString() === date.toDateString()) {
+                return 'selected';
+            }
+        }
+        if (selectFrom && selectTo) {
+            if (selectFrom < date && date < selectTo) {
+                return 'inRange';
+            }
+        }
+        return 'common';
+    }
 
-	getStatus(date, selectFrom, selectTo) {
-		if (selectFrom) {
-			if (selectFrom.toDateString() === date.toDateString()) {
-				return 'selected';
-			}
-		}
-		if (selectTo) {
-			if (selectTo.toDateString() === date.toDateString()) {
-				return 'selected';
-			}
-		}
-		if (selectFrom && selectTo) {
-			if (selectFrom < date && date < selectTo) {
-				return 'inRange';
-			}
-		}
-		return 'common';
-	}
+    render() {
+        let {style, isFutureDate} = this.props;
+        let directionStyles = {};
 
-	render() {
-		let {style, isFutureDate} = this.props;
-		let directionStyles = {};
+        if (!isFutureDate) {
+            directionStyles = {
+                transform: [{scaleY: -1}]
+            }
+        }
 
-		if (!isFutureDate) {
-			directionStyles = {
-				transform: [{scaleY: -1}]
-			}
-		}
-
-		return (
-			<ListView
-				initialListSize={5}
-				scrollRenderAheadDistance={1200}
-				style={[styles.listViewContainer, directionStyles, style]}
-				dataSource={this.state.dataSource}
-				renderRow={(month) => {
-					return (
-						<Month
-							{...this.props}
-							days={month}
-							style={[styles.month, directionStyles]}
-							changeSelection={this.changeSelection}
-						/>
-					);
-				}}
-			/>
-		);
-	}
+        return (
+            <ListView
+                initialListSize={5}
+                scrollRenderAheadDistance={1200}
+                style={[styles.listViewContainer, directionStyles, style]}
+                dataSource={this.state.dataSource}
+                renderRow={(month) => {
+                    return (
+                        <Month
+                            {...this.props}
+                            days={month}
+                            style={[styles.month, directionStyles]}
+                            changeSelection={this.changeSelection}
+                        />
+                    );
+                }}
+            />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-	listViewContainer: {
-		backgroundColor: 'white',
-		alignSelf: 'center',
-	},
-	month: {}
+    listViewContainer: {
+        backgroundColor: 'transparent',
+        alignSelf: 'center',
+    },
+    month: {}
 });
--- src/Month.js
+++ src/Month.js
@@ -1,84 +1,78 @@
 'use strict';
 
-import React    from 'react';
+import React from 'react';
 import {
-	View,
-	StyleSheet,
-	Text
-}               from 'react-native';
-import Day      from './Day'
+    View,
+    StyleSheet,
+    Text
+} from 'react-native';
+import Day from './Day'
 
 export default class Month extends React.Component {
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props);
 
-		this.weekDaysLocale = props.weekDaysLocale.slice();
+        this.weekDaysLocale = props.weekDaysLocale.slice();
 
-		if (props.startFromMonday) {
-			this.weekDaysLocale.push(this.weekDaysLocale.shift());
-		}
-	}
+        if (props.startFromMonday) {
+            this.weekDaysLocale.push(this.weekDaysLocale.shift());
+        }
+    }
 
-	render() {
-		let {
-			days, changeSelection, style, monthsLocale,
-			bodyBackColor, bodyTextColor, headerSepColor, width, monthTextColor
-		} = this.props;
+    render() {
+        let {
+            days, changeSelection, style, monthsLocale, daysTextColor,
+            bodyBackColor, bodyTextColor, headerSepColor, width, holidaysTextColor
+        } = this.props;
 
-		var monthHeader = monthsLocale[days[15].date.getMonth()] + ' ' + days[15].date.getFullYear();
+        var monthHeader = monthsLocale[days[15].date.getMonth()] + ' ' + days[15].date.getFullYear();
 
-		return (
-			<View style={[style, {width: width, backgroundColor: bodyBackColor}]}>
-				<Text style={[styles.monthHeader, {color: monthTextColor || bodyTextColor}]}>
-					{monthHeader}
-				</Text>
-				<View style={styles.monthDays}>
-					{this.weekDaysLocale.map((dayName, i) => {
-						return (
-							<View
-								key={i}
-								style={[styles.weekDay, {
-									borderColor: headerSepColor,
-									width: width / 7,
-									height: width / 7
-								}]}
-							>
-								<Text style={{color: bodyTextColor}}>{dayName}</Text>
-							</View>
-						);
-					})}
-					{days.map((day, i) => {
-						return (
-							<Day
-								key={i}
-								{...this.props}
-								disabled={day.disabled}
-								status={day.status}
-								date={day.date}
-								onDayPress={changeSelection}
-							/>
-						);
-					})}
-				</View>
-			</View>
-		);
-	}
+        return (
+            <View style={[style, {width: width, backgroundColor: bodyBackColor}]}>
+                <Text style={[styles.monthHeader, {color: bodyTextColor}]}>
+                    {monthHeader}
+                </Text>
+                <View style={styles.monthDays}>
+                    {this.weekDaysLocale.map((dayName, i) => {
+                        return (
+                            <View key={i}
+                                  style={[styles.weekDay, {borderColor: headerSepColor, width: width / 7, height: width / 7}]}>
+                                <Text style={{ color: i < 5 ? daysTextColor : holidaysTextColor }}>{dayName}</Text>
+                            </View>
+                        );
+                    })}
+                    {days.map((day, i) => {
+                        return (
+                            <Day
+                                key={i}
+                                {...this.props}
+                                disabled={day.disabled}
+                                status={day.status}
+                                date={day.date}
+                                onDayPress={changeSelection}
+                            />
+                        );
+                    })}
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-	monthHeader: {
-		marginTop: 15,
-		marginBottom: 5,
-		fontWeight: 'bold',
-		alignSelf: 'center'
-	},
-	monthDays: {
-		flexDirection: 'row',
-		flexWrap: 'wrap'
-	},
-	weekDay: {
-		borderBottomWidth: 1,
-		justifyContent: 'center',
-		alignItems: 'center'
-	}
+    monthHeader: {
+        marginTop: 15,
+        marginBottom: 5,
+        fontWeight: 'bold',
+        alignSelf: 'center'
+    },
+    monthDays: {
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    },
+    weekDay: {
+        borderBottomWidth: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
 });
